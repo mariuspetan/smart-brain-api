dd1d33a2c9a5a79cf39e5f9cc84feeb6fa47e1fe
mpetan <mpetan@connexservice.ca>
2019-05-13 23:08:33 -0400

    tweaking db connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : 'postgresql-sinuous-26660',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
